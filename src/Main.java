385208ac248ede271fb9636417f7287f60a04c22
warsmill <warsmill@yandex.ru>
2025-03-27 18:24:00 +0300

    leap years from 1904 to 2096

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,8 @@ public class Main {
         for ( int i = 10; i >= -10; i--){
             System.out.println(i);
         }
+        for (int i = 1904; i <= 2096; i+=4){
+            System.out.println(i);
+        }
     }
 }
