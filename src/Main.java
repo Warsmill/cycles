a624e1969ec38e51a9e59a697b0cab2726e13a4f
warsmill <warsmill@yandex.ru>
2025-03-27 18:25:45 +0300

    a program that outputs a sequence of numbers to the console

--- src/Main.java
+++ src/Main.java
@@ -18,5 +18,9 @@ public class Main {
         for (int i = 1904; i <= 2096; i+=4){
             System.out.println(i);
         }
+
+        for (int i = 7; i <= 98; i+=7){
+            System.out.println(i);
+        }
     }
 }
