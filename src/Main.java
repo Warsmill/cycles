be6c037ffeb01cf9a1c993e511569bf1f0d67c17
warsmill <warsmill@yandex.ru>
2025-03-27 18:20:05 +0300

    from 10 to 1

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         for(int i = 1; i <= 10 ; i++){
             System.out.println(i);
         }
+
+        for(int i = 10; i >=  1; i--){
+            System.out.println(i);
+        }
     }
 }
