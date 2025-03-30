d6a3389ea31c1005ee63e1e0759962e89a4dd5d4
warsmill <warsmill@yandex.ru>
2025-03-30 10:52:54 +0300

    annual amount of savings with interest

--- src/Main.java
+++ src/Main.java
@@ -37,7 +37,7 @@ public class Main {
         savings = 29_000;
         amountSavings = 0;
         for(int i = 1;i <= 12; i++){
-            amountSavings = (savings * i) + ((amountSavings / 100) * 1);
+            amountSavings = (int) (savings  + (amountSavings * 1.01));
             System.out.println("Месяц " + i + " ,сумма накоплений равна " + amountSavings + " рублей");
         }
     }
